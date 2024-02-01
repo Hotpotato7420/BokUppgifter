f5e3d0cd7039560830f85162c2d49cbadd2ef481
Hotpotato7420 <118818818+Hotpotato7420@users.noreply.github.com>
2024-02-01 10:56:37 +0100

    Add files via upload

---
+++ src/Kapitel_8/Uppgift8_11.java
@@ -0,0 +1,26 @@
+package Kapitel_8;
+import javax.swing.*;
+import java.util.Objects;
+
+
+ // INTE KLAR!!!!
+
+
+/*public class Uppgift8_11 {
+    public static void main(String[] args) {
+        String s = JOptionPane.showInputDialog("Skriv in din mening/ord");
+        String ur = Uppgift8_9.sarSkriv(s);
+
+        s = JOptionPane.showInputDialog("Skriv in ordet du vill soka efter");
+
+        boolean check = arAnagram(ur, s);
+        System.out.println(check);
+    }
+
+    public static boolean arAnagram(String s, String z) {
+        for (int i = 0; i < s.length(); i++) {
+               if (s.charAt(i) == )
+                }
+    }
+} */
+
